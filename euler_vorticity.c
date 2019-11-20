57977d2ff4f4cb498689080d149990c410291492
Ryan <ryan.smith@ucdconnect.ie>
2019-11-20 20:24:02 +0000

    Main function done, Need to tidy up code, Segfault at run.

--- euler_vorticity.c
+++ euler_vorticity.c
@@ -17,7 +17,6 @@ void print_array(const int Nx, const int Ny, long double *a){
     }
 }
 
-
 int main(){
     // Grid specifications
     const int Nx = 8;
@@ -41,11 +40,31 @@ int main(){
     for(int i=0;i<Ny;i++){
         yy[i]=i*dy;
     }
-    
+    // *************************************************************************
+    int kx[Nx];
+    int ky[Ny/2+1];
+    int k2[Nx][Ny/2+1];
+    int dealias[Nx][Ny/2+1];
+
     long double w[Nx][Ny];
     long double g[Nx][Ny];
     fftwl_complex w_hat[Nx][Ny/2+1];
     fftwl_complex g_hat[Nx][Ny/2+1];
+    long double avg_g2;
+
+    long double u[Nx][Ny]; 
+    long double v[Nx][Ny];
+    fftwl_complex v_hat[Nx][Ny/2+1];
+    fftwl_complex u_hat[Nx][Ny/2+1];
+
+    long double wx[Nx][Ny];
+    long double wy[Nx][Ny];
+    long double gx[Nx][Ny];
+    long double gy[Nx][Ny];
+    fftwl_complex wx_hat[Nx][Ny/2+1];
+    fftwl_complex wy_hat[Nx][Ny/2+1];
+    fftwl_complex gx_hat[Nx][Ny/2+1];
+    fftwl_complex gy_hat[Nx][Ny/2+1];
 
     // *************************************************************************
     // Plan the FFT & IFFT to increase performance in long run
@@ -71,11 +90,114 @@ int main(){
     printf("Setting initial conditions... \n");
     initial_conditions(Nx, Ny, xx, yy, *w, *g);
 
-    //Test FFT***
+    // Perform FFT on w, FFT is stored in w_hat 
     fftwl_execute(fft);
-    fftwl_execute(ift);
-    ift_scaling(Nx, Ny, *w);
-    print_array(Nx, Ny, *w);
+
+    // Matrices of wavenumbers
+    for(int i=0;i<Nx;i++) {
+        kx[i] = i;
+        for(int j=0;j<Ny/2+1;j++){
+            ky[j] = j;
+            k2[i][j] = kx[i]*kx[i] + ky[j]*ky[j];
+            if(kx[i]<2/3*(Nx/2) && ky[j]<2/3*(Ny/2)) {dealias[i][j] = 1;}
+        }
+    }
+    // Prevent dividing by 0 when solving for velocities in Fourier space
+    k2[0][0] = 1;
+    // Initialize the velocities
+
+    printf("Entering time loop... \n");
+    for(int iteration_time=0;iteration_time<n_timesteps;iteration_time++){
+        
+        for(int i=0;i<Nx;i++){
+            for(int j=0;i<Ny/2+1;j++){
+                // Real and complex parts stored in the [0] and [1] parts
+                u_hat[i][j][1] = (kx[i]*g_hat[i][j][0]+ky[j]*w_hat[i][j][0])/k2[i][j];
+                v_hat[i][j][1] = (ky[i]*g_hat[i][j][0]-kx[j]*w_hat[i][j][0])/k2[i][j];
+                u_hat[i][j][0] = -(kx[i]*g_hat[i][j][1]+ky[j]*w_hat[i][j][1])/k2[i][j];
+                v_hat[i][j][0] = -(ky[i]*g_hat[i][j][1]-kx[j]*w_hat[i][j][1])/k2[i][j];
+            }
+        }
+        // No zero mode
+        u_hat[0][0][0] = 0;
+        u_hat[0][0][1] = 0;
+        v_hat[0][0][0] = 0;
+        v_hat[0][0][1] = 0;
+        // Inverse transform and scaling of velocities
+        in_ift = *u_hat;
+        out_ift = *u;
+        fftwl_execute(ift);
+        in_ift = *v_hat;
+        out_ift = *v;
+        fftwl_execute(ift);
+        ift_scaling(Nx, Ny, *u);
+        ift_scaling(Nx, Ny, *v);
+
+        // Spatial average of gamma^2
+        avg_g2 = 0;
+        for(int i=0;i<Nx;i++){
+            for(int j=0;j<Ny;j++){
+                avg_g2 += (g[i][j])*(g[i][j]);
+            }
+        }
+        avg_g2 *= dx*dy/(4*M_PI*M_PI);
+        // Calculate derivatives for convection terms
+        for(int i=0;i<Nx;i++){
+            for(int j=0;j<Ny/2+1;j++){
+                wx_hat[i][j][0] = kx[i]*w_hat[i][j][1];
+                wx_hat[i][j][1] = -kx[i]*w_hat[i][j][0];
+                wy_hat[i][j][0] = ky[j]*w_hat[i][j][1];
+                wy_hat[i][j][1] = -ky[j]*w_hat[i][j][0];
+
+                gx_hat[i][j][0] = kx[i]*g_hat[i][j][1];
+                gx_hat[i][j][1] = -kx[i]*g_hat[i][j][0];
+                gy_hat[i][j][0] = ky[j]*g_hat[i][j][1];
+                gy_hat[i][j][1] = -ky[j]*g_hat[i][j][0];
+            }
+        }
+        in_ift = *wx_hat;
+        out_ift = *wx;
+        fftwl_execute(ift);
+
+        in_ift = *wy_hat;
+        out_ift = *wy;
+        fftwl_execute(ift);
+
+        in_ift = *gx_hat;
+        out_ift = *gy;
+        fftwl_execute(ift);
+
+        in_ift = *gy_hat;
+        out_ift = *gy;
+        fftwl_execute(ift);
+        ift_scaling(Nx, Ny, *wx);
+        ift_scaling(Nx, Ny, *wy);
+        ift_scaling(Nx, Ny, *gx);
+        ift_scaling(Nx, Ny, *gy);
+
+        // Update vorticity in real space
+        for(int i=0;i<Nx;i++){
+            for(int j=0;j<Ny;j++){
+                w[i][j] = dt*(g[i][j]*w[i][j]-u[i][j]*wx[i][j]-v[i][j]*wy[i][j])+w[i][j];
+                g[i][j] = dt*(2*avg_g2-g[i][j]*g[i][j]-u[i][j]*gx[i][j]-v[i][j]*gy[i][j])+g[i][j];
+            }
+        }
+        // Update the FFTs and dealiasing
+        in_fft = *w;
+        out_fft = *w_hat;
+        fftwl_execute(fft);
+        in_fft = *g;
+        out_fft = *g_hat;
+        fftwl_execute(fft);
+        for(int i=0;i<Nx;i++){
+            for(int j=0;j<Ny/2+1;j++){
+                w_hat[i][j][0] = w_hat[i][j][0]*dealias[i][j];
+                w_hat[i][j][1] = w_hat[i][j][1]*dealias[i][j];
+                g_hat[i][j][0] = g_hat[i][j][0]*dealias[i][j];
+                g_hat[i][j][1] = g_hat[i][j][1]*dealias[i][j];
+            }
+        }
+    }
     return 0;
 }
 
